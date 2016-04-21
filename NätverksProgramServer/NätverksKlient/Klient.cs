e7e42bd6d6ce08dd03bf6cd9e65884a5fcae6d09
evinus <tenold1@hotmail.com>
2016-04-21 18:22:00 +0200

    14

---
+++ NätverksProgramServer/NätverksKlient/Klient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NätverksKlient
+{
+    public partial class Klient : Form
+    {
+        TcpClient klient = new TcpClient();
+        public Klient()
+        {
+            InitializeComponent();
+            klient.NoDelay = true;
+        }
+
+         public async void StartSending ( string message )
+        {
+            if ( klient.Connected )
+            {
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    byte[] filData = System.IO.File.ReadAllBytes(openFileDialog1.FileName);
+
+                    try
+                    {
+                        await klient.GetStream().WriteAsync(filData, 0, filData.Length);
+                    }
+                    catch (Exception error) { MessageBox.Show(error.Message, "Klientfel"); return; }
+                }
+            }
+        }
+    }
+}
---
+++ NätverksProgramServer/NätverksProgramServer/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NätverksProgramServer
+{
+    public partial class Server : Form
+    {
+
+        List<TcpClient> klienter = new List<TcpClient>();
+        TcpListener lyssnare;
+        IPAddress hostAdress;
+        List<int> tal = new List<int>();
+        public Server()
+        {
+            InitializeComponent();
+            string hostNamn = Dns.GetHostName();
+        }
+        public async void StartaLyssna()
+        {
+            try
+            {
+               TcpClient klient = await lyssnare.AcceptTcpClientAsync();
+               klienter.Add(klient);
+               Lyssna(klient);
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show("kunde inte lysssna");
+                return;
+            }
+
+        }
+        public async void Lyssna(TcpClient client)
+        {
+
+        }
+    }
+}
