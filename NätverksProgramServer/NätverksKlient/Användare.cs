928cc9356be82e967b42dc1046b66c0094d49867
evinus <tenold1@hotmail.com>
2016-10-23 21:56:33 +0200

    NYA

--- NätverksProgramServer/NätverksKlient/Användare.cs
+++ NätverksProgramServer/NätverksKlient/Användare.cs
@@ -6,31 +6,46 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NätverksKlient
+
+public static class Användare
 {
-    public static class Användare
+    public static TcpClient klient = new TcpClient();
+    public static string Namn { get; set; }
+
+
+    public static async void Connect(string adress, int port, string namn)
     {
-        public static TcpClient klient = new TcpClient();
-        public static string Namn { get; set; }
-
-
-        
-      
-        public static async void Connect(string adress, int port, string namn)
+        Namn = namn;
+        byte[] _namn = Encoding.Unicode.GetBytes(Namn);
+        IPAddress Adress = IPAddress.Parse(adress);
+        try
         {
-            Namn = namn;
-            byte[] _namn = Encoding.Unicode.GetBytes(Namn);
-            IPAddress Adress = IPAddress.Parse(adress);
-            try
-            {
-                await klient.ConnectAsync(Adress, port);
-                await klient.GetStream().WriteAsync(_namn, 0, _namn.Length);
-            }
-            catch(Exception error)
-            {
-
-            }
+            await klient.ConnectAsync(Adress, port);
+            await klient.GetStream().WriteAsync(_namn, 0, _namn.Length);
         }
+        catch (Exception error)
+        {
 
+        }
     }
+    public static async void Skickameddelande(string meddelande)
+    {
+        try
+        {
+            //gör om talet 2 till bytes och skickar det
+            byte[] tal = BitConverter.GetBytes(2);
+            await klient.GetStream().WriteAsync(tal, 0, 4);
+            //gör om meddelandet till bytes och skickar det
+            byte[] medel = Encoding.Unicode.GetBytes(meddelande);
+            await klient.GetStream().WriteAsync(medel, 0, medel.Length);
+
+        }
+        catch (Exception error)//blir det fel
+        {
+            
+            return;
+        }
+    }
+
 }
+
