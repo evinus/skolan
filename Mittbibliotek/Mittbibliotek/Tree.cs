b88378437921a8df29c968f6d871131d05b5e43c
evinus <tenold1@hotmail.com>
2018-04-18 00:00:41 +0200

    0418

---
+++ Mittbibliotek/Mittbibliotek/Tree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mittbibliotek
+{
+    class Tree
+    {
+        private TreeNode root;
+
+        public Tree()
+        {
+            root = null;
+        }
+
+        public void InsertNode(int insertvalue)
+        {
+            if (root == null)
+                root = new TreeNode(insertvalue);
+            else
+                root.Insert(insertvalue);
+        }
+    }
+}
---
+++ Mittbibliotek/Mittbibliotek/TreeNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mittbibliotek
+{
+    class TreeNode
+    {
+
+        public TreeNode LeftNode { get; set; }
+
+        public int Data { get; set; }
+
+        public TreeNode RightNode { get; set; }
+
+        public TreeNode (int nodeData)
+        {
+            Data = nodeData;
+            LeftNode = RightNode = null;
+        }
+
+        public void Insert( int insertvalue)
+        {
+            if(insertvalue < Data)
+            {
+                if (LeftNode == null)
+                    LeftNode = new TreeNode(insertvalue);
+                else
+                    LeftNode.Insert(insertvalue);
+            }
+            else
+            {
+                if (RightNode == null)
+                    RightNode = new TreeNode(insertvalue);
+                else
+                    RightNode.Insert(insertvalue);
+            }
+        }
+    }
+}
